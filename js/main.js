b113a98d8bd413412436a6aed8c2aecee03262f0
Amdadul hasan Maruf <62442160+AHMARUF@users.noreply.github.com>
2023-08-05 12:10:47 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -70,14 +70,14 @@ $(document).ready(function(){
 
 
 
- window.onscroll = function() {scrollFunction()};
-  function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("myBtn").style.display = "block";
-  } else {
-    document.getElementById("myBtn").style.display = "none";
-  }
-}
+//  window.onscroll = function() {scrollFunction()};
+//   function scrollFunction() {
+//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//     document.getElementById("myBtn").style.display = "block";
+//   } else {
+//     document.getElementById("myBtn").style.display = "none";
+//   }
+// }
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0;
